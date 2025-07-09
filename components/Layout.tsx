5aba29297c38d6f8db466125ce4ba0318c2c6786
Denis <48854322+denn5551@users.noreply.github.com>
2025-07-09 12:32:47 +0700

    Remove trial banner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,10 @@
 // components/Layout.tsx
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [subscriptionStatus, setSubscriptionStatus] = useState<'trial' | 'active' | 'expired'>('expired');
-
-  useEffect(() => {
-    fetch('/api/me', { credentials: 'include' })
-      .then(res => res.json())
-      .then(data => setSubscriptionStatus(data.subscriptionStatus));
-  }, []);
-
-
   return (
     <>
       {/* lk-header will be rendered on account pages */}
-
-      {subscriptionStatus === 'trial' && (
-        <div className="trial-banner">
-          Чтобы пользоваться всеми ИИ-помощниками без ограничений, оформите подписку.
-        </div>
-      )}
 
       <main>{children}</main>
 
